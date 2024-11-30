9234c6f7b3627468262aa5a84f5432a8d2d1e401
GhaithSawan <ghaithx1x2x3@gmail.com>
2024-11-30 07:49:22 -0800

    dsa

--- frontEnd/src/component/Post/Post.jsx
+++ frontEnd/src/component/Post/Post.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import userimg from "../../assets/download.png";
 
 const Post = () => {
     return (
         <div>
-            
+            <div className="info flex w-100">
+                <div className='flex gap-1'>
+                    <img src={img} alt="" />
+                    <span>Ghaith</span>
+                </div>
+                <span>add friend</span>
+            </div>
+            <div className="desc">
+                djslakdjslajdosad;lasjkdj;aldj;sa
+            </div>
+            <img src={userimg} alt="" />
         </div>
     )
 }
