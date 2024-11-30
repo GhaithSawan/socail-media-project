84a8379a39e488c4be23090890917709a57b76e6
GhaithSawan <ghaithx1x2x3@gmail.com>
2024-11-30 07:41:42 -0800

    dsa

--- frontEnd/src/component/FriendList/FriendList.jsx
+++ frontEnd/src/component/FriendList/FriendList.jsx
@@ -3,11 +3,13 @@ import "./FriendList.css"
 import img from "../../assets/download.png"
 const FriendList = () => {
     return (
-        <div className='flex p-3' style={{flexDirection:"column",backgroundColor:"#181818"}}>
-            <h3>Friend list</h3>
-            <div className='FriendList'>
-                <img src={img} alt="" />
-                <span>Ghaith</span>
+        <div className='flex p-3' style={{ color: "#fff", flexDirection: "column", backgroundColor: "#181818" }}>
+            <h4>Friend list</h4>
+            <div className='FriendList flex '>
+                <div className='flex gap-1'>
+                    <img src={img} alt="" />
+                    <span>Ghaith</span>
+                </div>
                 <span>remove friend</span>
             </div>
         </div>
--- frontEnd/src/pages/Homepage.jsx
+++ frontEnd/src/pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import UserCard from '../component/UserCard/UserCard'
 import { CreatePost } from '../component/CreatePost/CreatePost'
+import FriendList from '../component/FriendList/FriendList'
 
 const Homepage = () => {
   return (
     <div>
       <UserCard />
       <CreatePost />
+      <FriendList />
     </div>
   )
 }
