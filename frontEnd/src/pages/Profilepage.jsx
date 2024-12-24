f9b885f34237b7a0d3fb33cdef2f410daaea1589
GhaithSawan <ghaithx1x2x3@gmail.com>
2024-12-24 00:26:18 -0800

    da

--- frontEnd/src/pages/Profilepage.jsx
+++ frontEnd/src/pages/Profilepage.jsx
@@ -3,9 +3,9 @@ import UserCard from '../component/UserCard/UserCard'
 import FriendList from '../component/FriendList/FriendList.jsx'
 import Posts from '../component/postscompontent/posts.jsx'
 import { CreatePost } from '../component/CreatePost/CreatePost.jsx'
-
+ 
 const Profilepage = () => {
-  return (
+  return ( 
     <div>Profilepage</div>
   )
 }
